6e2e2889923b6832742b704123f3ffe578375529
Stigma0310 <andherson.meza.roman@gmail.com>
2025-01-22 21:34:54 -0500

    Gordomit

---
+++ buscador.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Buscador Metal Con Ingenieros</title>
+<style>
+body{background:#fff;}
+</style>
+</head>
+<body>
+<script>
+  (function() {
+    var cx = '005934938102654738409:qnggxnaqibu';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+</body>
+</html>
